fa15d240c1296b393c60d87014056ecc15e8d533
Dany Paredes <danywalls4@gmail.com>
2025-07-31 14:58:43 +0200

    Refactor environment handling to support dynamic runtime and deployment configurations. Replace `env.json` with `.env` files for environment-specific API_URL settings. Update `ProductsService` to fetch API_URL from `EnvironmentConfigService`.

--- src/app/services/environment-config.service.ts
+++ src/app/services/environment-config.service.ts
@@ -2,16 +2,20 @@ import {inject, Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {tap} from "rxjs";
 
+
+
 @Injectable({ providedIn: 'root' })
 export class EnvironmentConfigService {
   private http = inject(HttpClient)
-  private env: any;
+  private env : Record<string, string> = {
+    API_URL: ''
+  };
 
   load() {
-    return this.http.get('/assets/env.json').pipe(
+    return this.http.get<Record<string, string>>('/assets/env.json').pipe(
       tap(data => {
         this.env = data;
-        if (!this.env?.API_URL) {
+        if (!this.env?.["API_URL"]) {
           console.warn('⚠️ Warning: API_URL is not defined in env.json');
         }
       })
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { environment} from "../../environments/environment";
 import {EnvironmentConfigService} from "./environment-config.service";
 
 export type Product = {
@@ -17,8 +16,8 @@ export type Product = {
 export class ProductsService {
 
   private http = inject(HttpClient)
-  private runtimeConfig = inject(EnvironmentConfigService)
-  private apiUrl = this.runtimeConfig.get('API_URL');
+  private environmentConfigService = inject(EnvironmentConfigService)
+  private apiUrl = this.environmentConfigService.get('API_URL');
   public products$ = this.http.get<Product[]>(this.apiUrl);
   public productById = (id: string) => this.http.get<Product>(`${this.apiUrl}/${id}`);
 }
